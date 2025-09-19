4d0f84233fcd59ced397830b5a95c9e2a2416e88
unknown <mauriciokun13@gmail.com>
2025-09-18 22:04:29 -0600

    expenses view and components html for now

---
+++ src/components/ExpensesForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>Add or update Expense</div>
+
+    <label>Expense Concept</label>
+    <input type="text">
+
+    <label>Amount</label>
+    <input type="number" step="0.01">
+
+    <label>Expense Type</label>
+    <select>
+        <option value="creditCard">Credit Card</option>
+        <option value="debitCard">Debit Card</option>
+        <option value="cash">Cash</option>
+        <option value="bankTransfer">Bank Transfer</option>
+    </select>
+
+    <label>Expense Date</label>
+    <input type="date">
+
+    <button>Save Expense</button>
+    <button>Cancel</button>
+</template>
+
+<script>export default{};</script>
--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -1,3 +1,65 @@
-<template>Expenses Page!</template>
+<template>
+    Expenses Page!
 
-<script>export default{};</script>
+     <div>filters
+
+        <label for="paymentType">Payment Type:</label>
+        <select id="paymentType" name="paymentType">
+            <option value="all">All</option>
+            <option value="creditCard">Credit Card</option>
+            <option value="debitCard">Debit Card</option>
+            <option value="cash">Cash</option>
+            <option value="bankTransfer">Bank Transfer</option>
+        </select>
+
+        <label for="dateRange">Date Range:</label>
+        <input type="date" id="startDate" name="startDate">
+        <input type="date" id="endDate" name="endDate">
+
+        <button>Apply Filters</button>
+        <button>Clear Filters</button>
+     </div>
+    
+    <div>Expenses Grid</div>
+    <table>
+        <tr>
+            <th>Concept</th>
+            <th>Amount</th>
+            <th>Payment Type</th>
+            <th>Payment Date</th>
+            <th>Action</th>
+        </tr>
+        <tr>
+            <td>Groceries</td>
+            <td>$150.00</td>
+            <td>Credit Card</td>
+            <td>2024-06-01</td>
+            <td><button>Edit</button> <button>Delete</button></td>
+        </tr>
+        <tr>
+            <td>Utilities</td>
+            <td>$75.00</td>
+            <td>Debit Card</td>
+            <td>2024-06-03</td>
+            <td><button>Edit</button> <button>Delete</button></td>
+        </tr>
+        <tr>
+            <td>Rent</td>
+            <td>$1200.00</td>
+            <td>Bank Transfer</td>
+            <td>2024-06-05</td>
+            <td><button>Edit</button> <button>Delete</button></td>
+        </tr> 
+    </table>
+
+    <button>new expense</button>
+
+    <expenses-form></expenses-form>
+
+    <modal-expense-delete></modal-expense-delete>
+</template>
+
+<script setup lang="ts">
+    import ExpensesForm from '../components/ExpensesForm.vue';
+    import ModalExpenseDelete from '../components/ModalExpenseDelete.vue';
+</script>
