041b6c92a21eca6e48e90c2c53ca0e45f1eb56ac
unknown <mauriciokun13@gmail.com>
2025-09-28 19:17:49 -0600

    login view html for now

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,18 @@
-<template>Login Page!</template>
+<template>
+    Login Page!
 
-<script>export default{};</script>
+    <form>
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <button type="submit">Login</button>
+    </form>
+    
+</template>
+
+<script setup lang="ts"></script>
